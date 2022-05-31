03896029c21409626f6f657bbf992ed2919f9906
Andrew Bernklau <andrewbernklau@hotmail.com>
2022-05-30 20:33:48 -0600

    Added an opening statement to home page and made iit responsive

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,12 @@
 const Home = () => {
   return (
-    <section class="home content-container">
+    <section className="home content-container">
+      <div className="hero">
+        <h1>
+          I'm baby vinyl kombucha gochujang next level. Fam raw denim 3 wolf
+          moon migas.
+        </h1>
+      </div>
       <div>
         <img class="home-img" src="images/cjs-home-img.jpg" alt="Nice Car" />
       </div>
