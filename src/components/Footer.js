971495ec52de6f91f871051029b291e1592c4966
abernklau1 <andrewbernklau@hotmail.com>
2022-10-03 22:16:59 -0600

    Set up Footer for navigation to other pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,36 @@
+import { BiCopyright } from "react-icons/bi";
+
 const Footer = () => {
   return (
     <section className="footer">
-      <div className="contacts"></div>
+      <div class="navigation">
+        <div class="services-nav">
+          <a href="/services"><h2>Services</h2></a>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+        <div class="contacts-nav">
+          <a href="/contact">Contact</a>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+        <div class="about-nav">
+          <a href="/about">About</a>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div className="contacts">
+        <div class="footer-icons">
+        </div>
+      </div>
+      <div class="copyright">
+        <BiCopyright />
+        Andrew Bernklau 2022
+      </div>
     </section>
   );
 };
